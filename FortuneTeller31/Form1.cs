6c8fc9680a82f2e5c3aa91d814717a99b738cf4d
pieunseo <s2318@e-mirim.hs.kr>
2025-08-25 14:14:00 +0900

    #1 내역 불러오기, 정보 폼 제작 및 호출 구현

--- FortuneTeller31/Form1.cs
+++ FortuneTeller31/Form1.cs
@@ -49,7 +49,8 @@ namespace FortuneTeller31
 
         private void 상담내역불러오기ToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            FormHistory form = new FormHistory();
+            form.Show();
         }
 
         private void 끝내기ToolStripMenuItem_Click(object sender, EventArgs e)
@@ -60,7 +61,8 @@ namespace FortuneTeller31
 
         private void 포춘텔러정보ToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            FormAbout form = new FormAbout();
+            form.ShowDialog();
         }
     }
 }
