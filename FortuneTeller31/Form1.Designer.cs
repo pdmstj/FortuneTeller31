bd75a6c22da8c7066de3e9e24fd3f803f8bab575
pieunseo <s2318@e-mirim.hs.kr>
2025-08-25 13:59:53 +0900

    #1 메뉴 구현

--- FortuneTeller31/Form1.Designer.cs
+++ FortuneTeller31/Form1.Designer.cs
@@ -36,8 +36,15 @@
             this.tbBirthtime = new System.Windows.Forms.TextBox();
             this.btnShowResult = new System.Windows.Forms.Button();
             this.tbResult = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.도움말ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.끝내기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.상담내역불러오기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.포춘텔러정보ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -47,19 +54,20 @@
             this.groupBox1.Controls.Add(this.tbBirthday);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Location = new System.Drawing.Point(12, 27);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(376, 426);
+            this.groupBox1.Size = new System.Drawing.Size(376, 411);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "사용자 입력";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.tbResult);
-            this.groupBox2.Location = new System.Drawing.Point(394, 12);
+            this.groupBox2.Location = new System.Drawing.Point(394, 27);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(394, 426);
+            this.groupBox2.Size = new System.Drawing.Size(394, 411);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "결과 확인";
@@ -67,26 +75,26 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(108, 83);
+            this.label1.Location = new System.Drawing.Point(79, 84);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(129, 12);
+            this.label1.Size = new System.Drawing.Size(209, 12);
             this.label1.TabIndex = 0;
-            this.label1.Text = "생년월일을 입력하세요";
+            this.label1.Text = "생년월일을 입력하세요 (ex.20070215)";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(89, 187);
+            this.label2.Location = new System.Drawing.Point(45, 187);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(157, 12);
+            this.label2.Size = new System.Drawing.Size(279, 12);
             this.label2.TabIndex = 1;
-            this.label2.Text = "태어난 시간을 입력해주세요";
+            this.label2.Text = "태어난 시간을 입력해주세요 (ex.새벽 3시 -> 3:00)";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // tbBirthday
             // 
-            this.tbBirthday.Location = new System.Drawing.Point(100, 115);
+            this.tbBirthday.Location = new System.Drawing.Point(111, 116);
             this.tbBirthday.Name = "tbBirthday";
             this.tbBirthday.Size = new System.Drawing.Size(137, 21);
             this.tbBirthday.TabIndex = 2;
@@ -94,14 +102,14 @@
             // 
             // tbBirthtime
             // 
-            this.tbBirthtime.Location = new System.Drawing.Point(100, 213);
+            this.tbBirthtime.Location = new System.Drawing.Point(111, 216);
             this.tbBirthtime.Name = "tbBirthtime";
             this.tbBirthtime.Size = new System.Drawing.Size(137, 21);
             this.tbBirthtime.TabIndex = 3;
             // 
             // btnShowResult
             // 
-            this.btnShowResult.Location = new System.Drawing.Point(37, 339);
+            this.btnShowResult.Location = new System.Drawing.Point(35, 319);
             this.btnShowResult.Name = "btnShowResult";
             this.btnShowResult.Size = new System.Drawing.Size(274, 56);
             this.btnShowResult.TabIndex = 4;
@@ -110,11 +118,62 @@
             // 
             // tbResult
             // 
-            this.tbResult.Location = new System.Drawing.Point(19, 21);
+            this.tbResult.BackColor = System.Drawing.SystemColors.Window;
+            this.tbResult.Location = new System.Drawing.Point(19, 20);
             this.tbResult.Multiline = true;
             this.tbResult.Name = "tbResult";
-            this.tbResult.Size = new System.Drawing.Size(356, 388);
+            this.tbResult.Size = new System.Drawing.Size(356, 373);
             this.tbResult.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.도움말ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.상담내역불러오기ToolStripMenuItem,
+            this.끝내기ToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(43, 20);
+            this.toolStripMenuItem1.Text = "파일";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // 도움말ToolStripMenuItem
+            // 
+            this.도움말ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.포춘텔러정보ToolStripMenuItem});
+            this.도움말ToolStripMenuItem.Name = "도움말ToolStripMenuItem";
+            this.도움말ToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.도움말ToolStripMenuItem.Text = "도움말";
+            // 
+            // 끝내기ToolStripMenuItem
+            // 
+            this.끝내기ToolStripMenuItem.Name = "끝내기ToolStripMenuItem";
+            this.끝내기ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.끝내기ToolStripMenuItem.Text = "끝내기";
+            this.끝내기ToolStripMenuItem.Click += new System.EventHandler(this.끝내기ToolStripMenuItem_Click);
+            // 
+            // 상담내역불러오기ToolStripMenuItem
+            // 
+            this.상담내역불러오기ToolStripMenuItem.Name = "상담내역불러오기ToolStripMenuItem";
+            this.상담내역불러오기ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.상담내역불러오기ToolStripMenuItem.Text = "내역 불러오기";
+            this.상담내역불러오기ToolStripMenuItem.Click += new System.EventHandler(this.상담내역불러오기ToolStripMenuItem_Click);
+            // 
+            // 포춘텔러정보ToolStripMenuItem
+            // 
+            this.포춘텔러정보ToolStripMenuItem.Name = "포춘텔러정보ToolStripMenuItem";
+            this.포춘텔러정보ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.포춘텔러정보ToolStripMenuItem.Text = "포춘텔러 정보";
+            this.포춘텔러정보ToolStripMenuItem.Click += new System.EventHandler(this.포춘텔러정보ToolStripMenuItem_Click);
             // 
             // FortuneTeller
             // 
@@ -123,14 +182,21 @@
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
             this.Name = "FortuneTeller";
-            this.Text = "FortuneTeller";
+            this.Text = "FortuneTeller 3115 피은서";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -144,6 +210,12 @@
         private System.Windows.Forms.TextBox tbBirthtime;
         private System.Windows.Forms.Button btnShowResult;
         private System.Windows.Forms.TextBox tbResult;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem 도움말ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 상담내역불러오기ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 끝내기ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 포춘텔러정보ToolStripMenuItem;
     }
 }
 
--- FortuneTeller31/Form1.cs
+++ FortuneTeller31/Form1.cs
@@ -36,5 +36,31 @@ namespace FortuneTeller31
         {
 
         }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 상담내역불러오기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 끝내기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+
+        }
+
+        private void 포춘텔러정보ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
